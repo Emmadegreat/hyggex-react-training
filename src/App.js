019b436fba125efc91414ecf046cad8dbeb55415
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2024-01-07 23:46:07 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-//import './App.css';
-
+import Childcomponent from "./child-component";
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-center text-blue-900 font-bold text-[3rem]">tailwind integration</h1>
+    <div className="mt-4 flex flex-col justify-center items-center bg-blue-500 md:bg-white">
+      <h1 className="text-center text-blue-900 font-bold text-[3rem]">My first react app</h1>
+      <b className="mt-2 text-[20px]">Tailwind integration in my first react app</b>
+      <Childcomponent child={"'I am a prop in a child component'"} />
     </div>
   );
 }
---
+++ src/child-component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Childcomponent = ({child}) => {
+    return (
+        <div>
+            <h3 className='py-8 text-cyan-500 font-400 text-[22px]'>Hello,  {child}</h3>
+
+        </div>
+    )
+}
+
+export default Childcomponent
